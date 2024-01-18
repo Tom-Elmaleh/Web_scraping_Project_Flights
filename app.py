0f96aeac320bd0bcef1cd98a6962f055a81dd52a
Tom0103 <elmalehtom@gmail.com>
2024-01-18 18:01:47 +0100

    Update web_app + data_merge + data_viz

--- app.py
+++ app.py
@@ -5,25 +5,11 @@ import plotly.express as px
 
 df = pd.read_csv("common_travels_data.csv",index_col=0)
 
-destinations = ['Venice-Naples', 'Venice-Bari', 'Milan-Bari', 'Milan-Naples','Milan-Pescara', 'Rome-Bari']
-vals_train = [1,2,3.5,2.5,2,2]
-
-df["carbon_footprint"] = 0
-
-for i in range(len(destinations)):
-    filt = (df['travel'] == destinations[i]) & (df['travel_mean']=='train')
-    df.loc[filt, 'carbon_footprint'] = vals_train[i]
-    
-vals_avions = [191,202,233,233,186,166]
-
-for i in range(len(destinations)):
-    filt = (df['travel'] == destinations[i]) & (df['travel_mean']=='plane')
-    df.loc[filt, 'carbon_footprint'] = vals_avions[i]
-
-
 # Affichage du dataframe shuffled
+st.write("### Aperçu du dataframe")
 st.dataframe(df.sample(frac=1, random_state=50).reset_index())
 
+# Visualisation prix du trajet
 st.write("### Comparaison du prix du trajet")
 fig = px.box(df, x='travel', y='price', color='travel_mean',color_discrete_sequence=px.colors.qualitative.Prism)
 fig.update_layout(title_text='Boîtes à moustache des prix (en euros) de chaque trajet pour chaque moyen de transport',
@@ -31,6 +17,7 @@ fig.update_layout(title_text='Boîtes à moustache des prix (en euros) de chaque
 fig.show()
 st.plotly_chart(fig)
 
+# Visualisation empreinte carbone
 st.write("### Comparaison de l'empreinte carbone du trajet")
 fig = px.bar(df.groupby(['travel', 'travel_mean'])['carbon_footprint'].max().reset_index(),
              x='travel', y='carbon_footprint', color='travel_mean',
@@ -40,6 +27,7 @@ fig.update_layout(title_text='Empreinte carbone (en kg) des différents trajets 
 fig.show()
 st.plotly_chart(fig)
 
+# Visualisation durée du trajet
 st.write("### Comparaison de la durée moyenne du trajet")
 fig = px.bar(df.groupby(['travel', 'travel_mean'])['travel_duration'].mean().reset_index(),
              x='travel', y='travel_duration', color='travel_mean',
